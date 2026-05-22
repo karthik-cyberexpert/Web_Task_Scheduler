15e6ff1b81f647a3bf567be447fe2508ea0d2b41
karthik-cyberexpert <itzkarthik.cyber@gmail.com>
2026-05-22 22:42:32 +0530

    Mobile App launched

--- supabase/functions/reset-firebase-password/index.js
+++ supabase/functions/reset-firebase-password/index.js
@@ -47,7 +47,7 @@ serve(async (req) => {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
     })
 
-  } catch (error: any) {
+  } catch (error) {
     console.error("Reset password error:", error);
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
