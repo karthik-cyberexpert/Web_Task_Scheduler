b2c41f865df99f49854eaa905bac7bbe8e7195c3
karthik-cyberexpert <itzkarthik.cyber@gmail.com>
2026-05-23 21:05:18 +0530

    Configure native Google Auth serverClientId client credentials for Android and Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.sydions.scheduler',
   appName: 'Sydions Scheduler',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '540302821731-ermnhis4l5n7ktghcosnku261otb1597.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
